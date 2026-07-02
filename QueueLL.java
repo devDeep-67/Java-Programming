1af13dcf5e94947e184a11d1d083c6af0468bf86
Deepanshu <deepanshupawar124@gmail.com>
2026-07-02 09:25:18 +0530

    n

---
+++ QueueLL.java
@@ -0,0 +1,73 @@
+public class QueueLL {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    static class Queue {
+        static Node head = null;
+        static Node tail = null;
+
+        // isEmpty
+        public static boolean isEmpty() {
+            return head == null && tail == null;
+        }
+
+        // add
+        public static void add(int data) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = tail = null;
+            }
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        // remove
+        public static int remove() {
+            if (isEmpty()) {
+                System.out.println("Queue is Empty!! ");
+                return -1;
+            }
+            int front = head.data;
+            // single element
+            if (tail == head) {
+                tail = head = null;
+            } else {
+                head = head.nextl;
+            }
+        }
+
+        // peek
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("Queue is Empty!! ");
+                return -1;
+            }
+            return head.data;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue(5);
+        q.add(7);
+        q.add(5);
+        q.add(1);
+        q.add(9);
+        while (!q.isEmpty()) {
+            System.out.print(q.peek() + " ");
+            q.remove();
+
+        }
+
+    }
+
+}
